bba0001947b33fd8382a9d0f60e01afdc88aa1a0
caikai <caikai@hikvision.com.cn>
2016-11-08 13:33:58 +0800

    my ex

--- myex.js
+++ myex.js
@@ -195,5 +195,5 @@
         }/*,
         url: '/info/help/studio/tips/ext/PicoBoard/'*/
     };
-    ScratchExtensions.register('PicoBoard', descriptor, ext, {type: 'serial'});
+    ScratchExtensions.register('', descriptor, ext, {type: 'serial'});
 })({});
